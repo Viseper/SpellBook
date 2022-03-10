82b86545cef7ccf22c2ccf510e5f6e4889932cf4
Viseper <Viseper@protonmail.com>
2022-03-09 22:08:05 -0700

    Added a new spell called 'Magic Missiles'. It summons 1-10 homing bolts that will hunt down the nearest entity and send it flying.

---
+++ src/main/java/me/viseper/spellbook/MagicMissiles.java
@@ -0,0 +1,60 @@
+package me.viseper.spellbook;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Particle;
+import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.*;
+import org.bukkit.event.player.PlayerHideEntityEvent;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.util.Vector;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MagicMissiles implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        Player player = (Player) sender;
+        if(player == null) {
+            System.out.println("Only a player can use this command");
+            return true;
+        }
+        int missileNumber = (int) Math.floor(Math.random() * 10) + 1;
+        World world = player.getWorld();
+        List<Arrow> arrows = new ArrayList<Arrow>();
+        for (int i = 0; i < missileNumber; i++) {
+            Arrow arrow = (Arrow) world.spawnEntity(new Location(world, player.getLocation().getX(), player.getLocation().getY() + 2, player.getLocation().getZ()), EntityType.ARROW);
+            arrow.setVelocity(player.getLocation().getDirection().multiply(new Vector(Math.random() * 2 + 1, Math.random() * 2 + 1, Math.random() * 2 + 1)));
+            arrow.addScoreboardTag("missile");
+            arrows.add(arrow);
+            player.hideEntity(player.getServer().getPluginManager().getPlugin("SpellBook"), arrow);
+            arrow.setKnockbackStrength(10);
+            new BukkitRunnable() {
+                @Override
+                public void run() {
+                    player.getWorld().spawnParticle(Particle.CRIT_MAGIC, arrow.getLocation(), 10);
+                    if(arrow.isInBlock() || arrow.isDead()) {
+                        cancel();
+                    } else {
+                        List<Entity> nearest = arrow.getNearbyEntities(20,20,20);
+                        for (Entity target: nearest) {
+                            if(player.hasLineOfSight(target) && target instanceof LivingEntity && !target.isDead() && target != player) {
+                                arrow.setVelocity(target.getLocation().toVector().subtract(arrow.getLocation().toVector().add(new Vector(0,1,0))).normalize());
+                            }
+                        }
+                    }
+                }
+            }.runTaskTimer(player.getServer().getPluginManager().getPlugin("SpellBook"), 0L, 1L);
+        }
+        return true;
+        }
+
+
+}
--- src/main/java/me/viseper/spellbook/SpellBook.java
+++ src/main/java/me/viseper/spellbook/SpellBook.java
@@ -4,18 +4,10 @@ import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.ComponentBuilder;
-import net.md_5.bungee.api.chat.HoverEvent;
-import net.md_5.bungee.api.chat.hover.content.Content;
-import org.bukkit.Bukkit;
-import org.bukkit.Material;
-import org.bukkit.NamespacedKey;
-import org.bukkit.entity.Item;
+import org.bukkit.*;
 import org.bukkit.inventory.*;
 import org.bukkit.inventory.meta.BookMeta;
 import org.bukkit.plugin.java.JavaPlugin;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.ArrayList;
 
 public final class SpellBook extends JavaPlugin {
 
@@ -27,8 +19,8 @@ public final class SpellBook extends JavaPlugin {
         ItemStack book = new ItemStack(Material.WRITTEN_BOOK);
         BookMeta bookMeta = (BookMeta) book.getItemMeta();
 
-        //bookMeta.setDisplayName("The Book of Spells");
-
+        //set up of spells
+        this.getCommand("magicMissiles").setExecutor(new MagicMissiles());
         //set up of the book
         //Example for future reference.
                 //BaseComponent[] page1 = new ComponentBuilder().append("Your Text Here").event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/kill @e[type=!player]")).event(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("This text shall kill everything.").create())).create();
@@ -37,13 +29,14 @@ public final class SpellBook extends JavaPlugin {
         BaseComponent[] page3 = new ComponentBuilder("Level 2 Fireball").color(ChatColor.DARK_RED).append("\n\n A slightly more complex spell that does a slightly more complex thing. Summons a medium sized fireball that could do some medium levels of damage.").color(ChatColor.RESET).append("\n\nSummon").color(ChatColor.DARK_AQUA).event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/summon minecraft:fireball ~ ~1 ~ {ExplosionPower:5}")).create();
         BaseComponent[] page4 = new ComponentBuilder("Level 3 Fireball").color(ChatColor.DARK_RED).append("\n\n A very  more complex spell that does a very complex thing. Summons a massive fireball that could do some serious damage.").color(ChatColor.RESET).append("\n\nSummon").color(ChatColor.DARK_AQUA).event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/summon minecraft:fireball ~ ~1 ~ {ExplosionPower:10}")).create();
         BaseComponent[] page5 = new ComponentBuilder("Doomsday").color(ChatColor.DARK_RED).append("\n\nThis will kill you. There is no escaping this fireball. This spell will end everything. You, your allies, and your enemies. Do not use!").color(ChatColor.RESET).append("\n\nEnd everything").color(ChatColor.DARK_AQUA).event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/summon minecraft:fireball ~ ~1 ~ {ExplosionPower:127}")).create();
-        BaseComponent[] page6 = new ComponentBuilder("Spell Name").color(ChatColor.DARK_RED).append("\n\nSample Text").color(ChatColor.RESET).append("\n\nActivate Spell").color(ChatColor.DARK_AQUA).event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/summon minecraft:tnt")).create();
+        BaseComponent[] page6 = new ComponentBuilder("Magic Missiles").color(ChatColor.AQUA).append("\n\nSummons an array of magic missiles.").color(ChatColor.RESET).append("\n\nActivate Spell").color(ChatColor.DARK_AQUA).event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/magicMissiles")).create();
 
         bookMeta.spigot().addPage(page1);
         bookMeta.spigot().addPage(page2);
         bookMeta.spigot().addPage(page3);
         bookMeta.spigot().addPage(page4);
         bookMeta.spigot().addPage(page5);
+        bookMeta.spigot().addPage(page6);
         bookMeta.setAuthor("Viseper");
         bookMeta.setTitle("The Book of Spells");
         book.setItemMeta(bookMeta);
@@ -60,5 +53,8 @@ public final class SpellBook extends JavaPlugin {
 
 
     }
+
+
+
 
 }
