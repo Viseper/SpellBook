edf17418312dc17bcbfc72a4ca84046153d91e85
BuildTools <unconfigured@null.spigotmc.org>
2022-05-04 14:24:16 -0700

    Added the table of contents

--- src/main/java/me/viseper/spellbook/SpellBook.java
+++ src/main/java/me/viseper/spellbook/SpellBook.java
@@ -25,18 +25,18 @@ public final class SpellBook extends JavaPlugin {
         //Example for future reference.
                 //BaseComponent[] page1 = new ComponentBuilder().append("Your Text Here").event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/kill @e[type=!player]")).event(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("This text shall kill everything.").create())).create();
         BaseComponent[] page1 = new ComponentBuilder("Welcome to the ancient book of spells.").color(ChatColor.DARK_PURPLE).append("\n \n \n \n").append("Written by ").append("Viseper").obfuscated(true).create();
-        BaseComponent[] page2 = new ComponentBuilder("Fireball").color(ChatColor.DARK_RED).append("\n\n A simple spell that does a simple thing. Summons a small fireball that could do some minor damage.").color(ChatColor.RESET).append("\n\nSummon").color(ChatColor.DARK_AQUA).event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/summon minecraft:fireball ~ ~1 ~ {ExplosionPower:1}")).create();
-        BaseComponent[] page3 = new ComponentBuilder("Level 2 Fireball").color(ChatColor.DARK_RED).append("\n\n A slightly more complex spell that does a slightly more complex thing. Summons a medium sized fireball that could do some medium levels of damage.").color(ChatColor.RESET).append("\n\nSummon").color(ChatColor.DARK_AQUA).event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/summon minecraft:fireball ~ ~1 ~ {ExplosionPower:5}")).create();
-        BaseComponent[] page4 = new ComponentBuilder("Level 3 Fireball").color(ChatColor.DARK_RED).append("\n\n A very  more complex spell that does a very complex thing. Summons a massive fireball that could do some serious damage.").color(ChatColor.RESET).append("\n\nSummon").color(ChatColor.DARK_AQUA).event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/summon minecraft:fireball ~ ~1 ~ {ExplosionPower:10}")).create();
-        BaseComponent[] page5 = new ComponentBuilder("Doomsday").color(ChatColor.DARK_RED).append("\n\nThis will kill you. There is no escaping this fireball. This spell will end everything. You, your allies, and your enemies. Do not use!").color(ChatColor.RESET).append("\n\nEnd everything").color(ChatColor.DARK_AQUA).event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/summon minecraft:fireball ~ ~1 ~ {ExplosionPower:127}")).create();
-        BaseComponent[] page6 = new ComponentBuilder("Magic Missiles").color(ChatColor.AQUA).append("\n\nSummons an array of magic missiles.").color(ChatColor.RESET).append("\n\nActivate Spell").color(ChatColor.DARK_AQUA).event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/magicMissiles")).create();
+        BaseComponent[] page2 = new ComponentBuilder("SpellBook Contents").color(ChatColor.GOLD).append("\n\nFireball").color(ChatColor.RESET).event(new ClickEvent(ClickEvent.Action.CHANGE_PAGE, "3")).append("\n\nMagic Missiles").event(new ClickEvent(ClickEvent.Action.CHANGE_PAGE, "4")).create();
+        BaseComponent[] page3 = new ComponentBuilder("Fireball").color(ChatColor.DARK_RED).append("\n\n A decently powerful spell that summons a ball of fire.").color(ChatColor.RESET).append("\n\nSummon").color(ChatColor.DARK_AQUA).event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/summon minecraft:fireball ~ ~1 ~ {ExplosionPower:10}")).create();
+        //ignore
+        //BaseComponent[] page4 = new ComponentBuilder("Level 3 Fireball").color(ChatColor.DARK_RED).append("\n\n A very  more complex spell that does a very complex thing. Summons a massive fireball that could do some serious damage.").color(ChatColor.RESET).append("\n\nSummon").color(ChatColor.DARK_AQUA).event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/summon minecraft:fireball ~ ~1 ~ {ExplosionPower:10}")).create();
+        //Do not enable this spell unless absolutely necessary
+        //BaseComponent[] page5 = new ComponentBuilder("Doomsday").color(ChatColor.DARK_RED).append("\n\nThis will kill you. There is no escaping this fireball. This spell will end everything. You, your allies, and your enemies. Do not use!").color(ChatColor.RESET).append("\n\nEnd everything").color(ChatColor.DARK_AQUA).event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/summon minecraft:fireball ~ ~1 ~ {ExplosionPower:127}")).create();
+        BaseComponent[] page4 = new ComponentBuilder("Magic Missiles").color(ChatColor.AQUA).append("\n\nSummons an array of magic missiles.").color(ChatColor.RESET).append("\n\nActivate Spell").color(ChatColor.DARK_AQUA).event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/magicMissiles")).create();
 
         bookMeta.spigot().addPage(page1);
         bookMeta.spigot().addPage(page2);
         bookMeta.spigot().addPage(page3);
         bookMeta.spigot().addPage(page4);
-        bookMeta.spigot().addPage(page5);
-        bookMeta.spigot().addPage(page6);
         bookMeta.setAuthor("Viseper");
         bookMeta.setTitle("The Book of Spells");
         book.setItemMeta(bookMeta);
