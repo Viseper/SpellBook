cd3824e994a6cf0a1af53bc5d23baefa428f6ed0
BuildTools <unconfigured@null.spigotmc.org>
2022-05-09 14:55:43 -0600

    Shield bug fixed, now storage containers will no longer lose their stored items when the spell ends

--- src/main/java/me/viseper/spellbook/Shield.java
+++ src/main/java/me/viseper/spellbook/Shield.java
@@ -15,6 +15,7 @@ import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Shulker;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
 import sun.jvm.hotspot.oops.Metadata;
@@ -37,6 +38,7 @@ public class Shield implements CommandExecutor {
         Material[] materials = new Material[6 * (5 * 2)];
         Location l = player.getLocation();
         // example: player.getLocation().getBlock().setType(Material.OBSIDIAN);
+        ArrayList<Inventory> inv = getContainer(pLoc, 5);
         ArrayList<Block> blocks = getData(pLoc, 5);
         ArrayList<Material> blockList = getBlocks(pLoc, 5, Material.OBSIDIAN);
         ArrayList<Material> insideBlock = getBlocks(pLoc, 4, Material.AIR);
@@ -47,15 +49,16 @@ public class Shield implements CommandExecutor {
             public void run() {
                 int arrayLocation = 0;
                 int arrayLocation2 = 0;
+                int arrayLocation3 = 0;
                 for(double x = pLoc.getX() - 5; x <= pLoc.getX() + 5; x++){
                     for(double y = pLoc.getY() - 5; y <= pLoc.getY() + 5; y++){
                         for(double z = pLoc.getZ() - 5; z <= pLoc.getZ() + 5; z++){
                             Location loc = new Location(pLoc.getWorld(), x, y, z);
-                            if(loc.getBlock() instanceof Chest || loc.getBlock() instanceof CreatureSpawner || loc.getBlock() instanceof ShulkerBox || loc.getBlock() instanceof DoubleChest || loc.getBlock() instanceof Furnace || loc.getBlock() instanceof Hopper || loc.getBlock() instanceof Dropper || loc.getBlock() instanceof Dispenser) {
-                                continue;
-                            }
                             loc.getBlock().setType(blockList.get(arrayLocation++));
-                            loc.getBlock().setBlockData(blocks.get(arrayLocation2++).getBlockData());
+                            //loc.getBlock().setBlockData(blocks.get(arrayLocation2++).getBlockData());
+                            if(loc.getBlock().getState() instanceof Container) {
+                                ((Container) loc.getBlock().getState()).getInventory().setContents(inv.get(arrayLocation3++).getContents());
+                            }
                         }
                     }
                 }
@@ -64,52 +67,6 @@ public class Shield implements CommandExecutor {
         }.runTaskLater(player.getServer().getPluginManager().getPlugin("SpellBook"), 100L);
         return true;
     }
-    /* Just gonna disable this until we can come up with a better method for a sphere.
-    public void makeCircle(Location loc, Double r, Material m) {
-        for (double i = 0; i <= Math.PI; i += Math.PI / r) {
-            double radius = Math.sin(i) * r;
-            double y = Math.cos(i) * r;
-            for (double a = 0; a < Math.PI * 2; a+= Math.PI / r) {
-                double x = Math.cos(a) * radius;
-                double z = Math.sin(a) * radius;
-                loc.add(x, y, z);
-                loc.getBlock().setType(m);
-                loc.subtract(x, y, z);
-            }
-        }
-    }
-
-    public double[][] storeCircle(Location location, int r) {
-        double[][] coordinates = new double[(r + 1) * (r * 2)][];
-        int arrayLocation = 0;
-        for (double i = 0; i <= Math.PI; i += Math.PI / r) {
-            double radius = Math.sin(i) * r;
-            double y = Math.cos(i) * r;
-            for (double a = 0; a < Math.PI * 2; a+= Math.PI / r) {
-                double x = Math.cos(a) * radius;
-                double z = Math.sin(a) * radius;
-                location.add(x, y, z);
-                coordinates[arrayLocation++] = new double[] { x, y, z };
-                location.subtract(x, y, z);
-            }
-        }
-        return coordinates;
-    }
-
-    public void restoreCircle(Location loc, Double r, Material[] m) {
-        int arrayLocation = 0;
-        for (double i = 0; i <= Math.PI; i += Math.PI / r) {
-            double radius = Math.sin(i) * r;
-            double y = Math.cos(i) * r;
-            for (double a = 0; a < Math.PI * 2; a+= Math.PI / r) {
-                double x = Math.cos(a) * radius;
-                double z = Math.sin(a) * radius;
-                loc.add(x, y, z);
-                loc.getBlock().setType(m[arrayLocation++]);
-                loc.subtract(x, y, z);
-            }
-        }
-    } */
 
     public ArrayList<Material> getBlocks(Location start, int radius, Material m){
         ArrayList<Material> blocks = new ArrayList<Material>();
@@ -117,9 +74,6 @@ public class Shield implements CommandExecutor {
             for(double y = start.getY() - radius; y <= start.getY() + radius; y++){
                 for(double z = start.getZ() - radius; z <= start.getZ() + radius; z++){
                     Location loc = new Location(start.getWorld(), x, y, z);
-                    if(loc.getBlock() instanceof Chest || loc.getBlock() instanceof CreatureSpawner || loc.getBlock() instanceof ShulkerBox || loc.getBlock() instanceof DoubleChest || loc.getBlock() instanceof Furnace || loc.getBlock() instanceof Hopper || loc.getBlock() instanceof Dropper || loc.getBlock() instanceof Dispenser) {
-                        continue;
-                    }
                     blocks.add(loc.getBlock().getType());
                     loc.getBlock().setType(m);
                 }
@@ -137,7 +91,21 @@ public class Shield implements CommandExecutor {
                 for (double z = start.getZ() - radius; z <= start.getZ() + radius; z++) {
                     Location loc = new Location(start.getWorld(), x, y, z);
                     blocks.add(loc.getBlock());
+                }
+            }
+        }
+        return blocks;
+    }
 
+    public ArrayList<Inventory> getContainer(Location start, int radius) {
+        ArrayList<Inventory> blocks = new ArrayList<>();
+        for (double x = start.getX() - radius; x <= start.getX() + radius; x++) {
+            for (double y = start.getY() - radius; y <= start.getY() + radius; y++) {
+                for (double z = start.getZ() - radius; z <= start.getZ() + radius; z++) {
+                    Location loc = new Location(start.getWorld(), x, y, z);
+                    if(loc.getBlock().getState() instanceof Container) {
+                        blocks.add(((Container) loc.getBlock().getState()).getInventory());
+                    }
                 }
             }
         }
